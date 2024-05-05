5ed6d72fd9553190c50e34b87fe5dfc4c213aeed
acostaDemianAaron <acostademiann14@gmail.com>
2024-05-05 20:37:46 -0300

    Se agrego el componente Search y funciona. Falta ordenar el codigo

--- restaurant-react/src/app/components/FoodList/FoodList.jsx
+++ restaurant-react/src/app/components/FoodList/FoodList.jsx
@@ -1,16 +1,24 @@
 "use client";
+import React, { useState, useEffect } from 'react';
 import useFoodHooks from "../../hooks/useFoodHooks";
 import FoodItem from "../FoodItem/FoodItem";
-
+import FoodSearch from "../FoodSearch/FoodSearch";
 
 export const FoodList = () => {
     const { foods } = useFoodHooks();
+    const [filteredFoods, setFilteredFoods] = useState([]);
+
+    useEffect(() => {
+        setFilteredFoods(foods);
+    }, [foods]);
 
     return (
         <div>
-            {foods.map((food) => (
+            <FoodSearch foods={foods} setFilteredFoods={setFilteredFoods} />
+            {filteredFoods.map((food) => (
                 <FoodItem key={food.id} food={food} />
             ))}
         </div>
     );
 };
+
