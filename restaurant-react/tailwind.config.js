9e087e3cd2fab61d01a54b1a08dffb280901c7d7
Alan Cristian Gaston Vera <93807967+veraAlan@users.noreply.github.com>
2024-05-06 22:30:28 -0300

    Small changes Footer and Nav

--- restaurant-react/tailwind.config.js
+++ restaurant-react/tailwind.config.js
@@ -11,17 +11,7 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      animation: {
-        "flip": "flip 1s ease-out",
-      },
-      keyframes: {
-        flip: {
-          '0%': { transform: 'rotateY(0deg)', opacity: 1 },
-          '50%': { transform: 'rotateY(90deg)', opacity: 0.2 },
-          '100%': { transform: 'rotateY(180deg)', opacity: 0 }
-        }
-      },
+      }
     },
   },
   plugins: [],
